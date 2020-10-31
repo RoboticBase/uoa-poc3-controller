339c9ece531f535773596026e6d1260656eca04b
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2020-11-01 08:36:26 +0900

    added vue.ts boilerplate

---
+++ order-ui/src/router/index.ts
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
