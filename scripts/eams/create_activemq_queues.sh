25c272272ab9bee91cd38160b4824c7141e532aa
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2020-10-31 17:15:59 +0900

    updated scripts for static-route-planner

---
+++ scripts/eams/create_activemq_queues.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cmd="/var/lib/artemis/bin/artemis"
+user=artemis
+password=simetraehcapa
+type=robot
+
+ids=("robot01")
+suffixes=("up" "down")
+for id in "${ids[@]}"; do
+  for suf in "${suffixes[@]}"; do
+    docker exec -t static-planner_activemq_1 \
+      ${cmd} queue create --user ${user} --password ${password} \
+      --name ${type}.${id}.${suf} --address ${type}.${id}.${suf} \
+      --anycast --no-durable --preserve-on-no-consumers --auto-create-address
+  done 
+done
+
