29046d8be6d3986d94dc49108eeef5cfb4a9d6a2
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2020-10-31 17:15:07 +0900

    refactored StaticRoutePlanner

--- static-planner/app/src/api.py
+++ static-planner/app/src/api.py
@@ -18,9 +18,12 @@ class StaticRoutePlanner(MethodView):
         logger.debug('StaticRoutePlanner.post')
         body = request.json
 
-        if body is None or body.get('planId') is None:
+        if body is None or body.get('planId') is None or body.get('robotId') is None:
+            msg = f'"planId" and/or "robotId" do not exist, body={body}'
+            logger.warning(f'status=400, {msg}')
             abort(400, {
-                'message': f'"planId" does not found, body={body}',
+                'result': 'failure',
+                'message': msg,
             })
 
         plan_id = body['planId']
@@ -28,19 +31,24 @@ class StaticRoutePlanner(MethodView):
         entity = orion.get_entity(const.FIWARE_SERVICE, const.FIWARE_SERVICEPATH, const.PLAN_TYPE, plan_id)
         payload = self._make_cmd(entity['waypoints']['value'])
         result = orion.send_command(const.FIWARE_SERVICE, const.FIWARE_SERVICEPATH, const.ROBOT_TYPE, robot_id, payload)
+
         logger.info(f'send a "{const.START_COMMAND}" command to orion, '
                     f'result_status={result.status_code}, payload={json.dumps(payload)}')
 
         return jsonify({
             'result': 'success',
             'planId': plan_id,
+            'robotId': robot_id,
             'orion_status': result.status_code,
         }), 201
 
     def _make_cmd(self, waypoints):
         if not isinstance(waypoints, list):
+            msg = f'retrieved waypoints is not list, waypoints={waypoints}'
+            logger.error(f'status=500, {msg}')
             abort(500, {
-                'message': f'retrieved waypoints is not list, waypoints={waypoints}'
+                'result': 'failure',
+                'message': msg,
             })
         t = datetime.datetime.now(const.TIMEZONE).isoformat(timespec='milliseconds')
 
