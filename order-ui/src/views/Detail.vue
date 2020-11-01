396bbfb0620a7f1229718f0aeaece5bf63e8a98d
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2020-11-01 16:42:16 +0900

    updated the Detail view in order to validate the reservation count

--- order-ui/src/views/Detail.vue
+++ order-ui/src/views/Detail.vue
@@ -8,9 +8,9 @@
         <div class="card-body px-2 py-3">
           <h5 class="card-title">{{ stock.title }}</h5>
           <p class="card-text">カテゴリ：{{ stock.category }}</p>
-          <p class="card-text">価格（税込み）：{{ parseInt(stock.price)}}円</p>
-          <p class="card-text">在庫数：{{ parseInt(stock.quantity)}}個</p>
-          <p class="card-text form-group">注文数：<input type="number" class="form-control reservation" v-model.number="reservation"/></p>
+          <p class="card-text">価格（税込み）：{{ stock.price }}円</p>
+          <p class="card-text">在庫数：{{ stock.quantity }}個</p>
+          <p class="card-text form-group">注文数：<input type="number" min="0" :max="stock.quantity" class="form-control reservation" v-model.number="reservation"/></p>
           <p class="mb-0 form-group"><b-button @click="reserve" variant="outline-primary" class="reserve">カートに入れる</b-button></p>
         </div>
       </div>
@@ -59,9 +59,16 @@ export default Vue.extend({
   },
   methods: {
     reserve(): void {
-      this.stock.reservation = this.reservation;
-      this.$store.commit('updateStock', {idx: this.idx, stock: this.stock});
-      this.$router.push({name: 'stocks'});
+      if (this.reservation <= 0) {
+        this.$store.commit('updateMessage', {message: '0以下の数は入力できません', variant: 'warning'})
+      }
+      else if (this.reservation > this.stock.quantity) {
+        this.$store.commit('updateMessage', {message: '予約数が在庫数を超えています', variant: 'warning'})
+      } else {
+        this.stock.reservation = this.reservation;
+        this.$store.commit('updateStock', {idx: this.idx, stock: this.stock});
+        this.$router.push({name: 'stocks'});
+      }
     }
   },
 });
